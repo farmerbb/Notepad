eed6403d371b1d1fd98702d7d1f285d9ea6e4af3
farmerbb <farmerbb@gmail.com>
2015-02-08 18:18:23 -0700

    Notepad 2.1.4
    Use Android's built-in GestureDetector instead of manually attempting to
    detect double-taps (fixes oddities with text selection).  Layout cleanup
    (remove unnecessary TextViews)

--- src/com/farmerbb/notepad/NoteViewFragment.java
+++ src/com/farmerbb/notepad/NoteViewFragment.java
@@ -25,11 +25,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -44,7 +46,7 @@ public class NoteViewFragment extends Fragment {
 	String filename = "";
 	String contentsOnLoad = "";
 	int firstLoad;
-	long lastTapTime = 0;
+    boolean showMessage = true;
 
     // Receiver used to close fragment when a note is deleted
     public class DeleteNotesReceiver extends BroadcastReceiver {
@@ -106,96 +108,138 @@ public class NoteViewFragment extends Fragment {
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
+        super.onActivityCreated(savedInstanceState);
 
         // Set values
         setRetainInstance(true);
-		setHasOptionsMenu(true);
+        setHasOptionsMenu(true);
 
         // Change window title
         getActivity().setTitle(getResources().getString(R.string.view_note));
 
-		// Show the Up button in the action bar.
+        // Show the Up button in the action bar.
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Set up content view
         noteContents = (TextView) getActivity().findViewById(R.id.textView);
-		
-		// Get filename of saved note
-		filename = getArguments().getString("filename");
 
-		// Load note contents
-		try {
-			contentsOnLoad = listener.loadNote(filename);
-		} catch (IOException e) {
-			showToast(R.string.error_loading_note);
+        // Get filename of saved note
+        filename = getArguments().getString("filename");
 
-			// Add NoteListFragment or WelcomeFragment
-			Fragment fragment;
-			if(getActivity().findViewById(R.id.layoutMain).getTag().equals("main-layout-normal"))
-				fragment = new NoteListFragment();
-			else
-				fragment = new WelcomeFragment();
+        // Load note contents
+        try {
+            contentsOnLoad = listener.loadNote(filename);
+        } catch (IOException e) {
+            showToast(R.string.error_loading_note);
 
-			getFragmentManager()
-				.beginTransaction()
-				.replace(R.id.noteViewEdit, fragment, "NoteListFragment")
-				.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
-				.commit();
-		}
-		
-		// Set TextView contents
-		noteContents.setText(contentsOnLoad);
-			
-		// Show a toast message if this is the user's first time viewing a note
-		SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-		firstLoad = sharedPref.getInt("first-load", 0);
-		if(firstLoad == 0) {
-			// Show dialog with info
-			DialogFragment firstLoad = new FirstViewDialogFragment();
-			firstLoad.show(getFragmentManager(), "firstloadfragment");
+            // Add NoteListFragment or WelcomeFragment
+            Fragment fragment;
+            if(getActivity().findViewById(R.id.layoutMain).getTag().equals("main-layout-normal"))
+                fragment = new NoteListFragment();
+            else
+                fragment = new WelcomeFragment();
 
-			// Set first-load preference to 1; we don't need to show the dialog anymore
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.putInt("first-load", 1);
-			editor.apply();
-		}
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.noteViewEdit, fragment, "NoteListFragment")
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                    .commit();
+        }
 
-		// Make TextView edit note onClick for convenience
-		noteContents.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-				long tapTime = System.currentTimeMillis();
-				boolean showMessage = true;
-				
-				if(tapTime - lastTapTime < 500) {
-					if(sharedPref.getBoolean("show_double_tap_message", true)) {
-						SharedPreferences.Editor editor = sharedPref.edit();
-						editor.putBoolean("show_double_tap_message", false);
-						editor.apply();
-					}
-					
-					Bundle bundle = new Bundle();
-					bundle.putString("filename", filename);
+        // Set TextView contents
+        noteContents.setText(contentsOnLoad);
 
-					Fragment fragment = new NoteEditFragment();
-					fragment.setArguments(bundle);
+        // Show a toast message if this is the user's first time viewing a note
+        final SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        firstLoad = sharedPref.getInt("first-load", 0);
+        if(firstLoad == 0) {
+            // Show dialog with info
+            DialogFragment firstLoad = new FirstViewDialogFragment();
+            firstLoad.show(getFragmentManager(), "firstloadfragment");
 
-					getFragmentManager()
-						.beginTransaction()
-						.replace(R.id.noteViewEdit, fragment, "NoteEditFragment")
-						.commit();
-				} else {
-					lastTapTime = tapTime;
-					if(sharedPref.getBoolean("show_double_tap_message", true) && showMessage) {
-						showToastLong(R.string.double_tap);
-						showMessage = false;
-					}
-				}
-			}
-		});
-	}
+            // Set first-load preference to 1; we don't need to show the dialog anymore
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("first-load", 1);
+            editor.apply();
+        }
+
+        // Detect single and double-taps using GestureDetector
+        final GestureDetector detector = new GestureDetector(getActivity(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {}
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {}
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+            }
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+        });
+
+        detector.setOnDoubleTapListener(new GestureDetector.OnDoubleTapListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                if(sharedPref.getBoolean("show_double_tap_message", true)) {
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean("show_double_tap_message", false);
+                    editor.apply();
+                }
+
+                Bundle bundle = new Bundle();
+                bundle.putString("filename", filename);
+
+                Fragment fragment = new NoteEditFragment();
+                fragment.setArguments(bundle);
+
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.noteViewEdit, fragment, "NoteEditFragment")
+                        .commit();
+
+                return false;
+            }
+
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(MotionEvent e) {
+                if(sharedPref.getBoolean("show_double_tap_message", true) && showMessage) {
+                    showToastLong(R.string.double_tap);
+                    showMessage = false;
+                }
+
+                return false;
+            }
+
+        });
+
+        noteContents.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return false;
+            }
+        });
+    }
 
     // Register and unregister DeleteNotesReceiver
     @Override

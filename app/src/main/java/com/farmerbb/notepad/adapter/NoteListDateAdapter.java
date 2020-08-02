0f86ca34fbb9d32461c34c040df82338583db8cb
Max Große <max12.gro@gmail.com>
2020-08-02 19:48:34 +0200

    Realize UI changes with custom methods
    Replaced duplicate code with function calls to ThemeManager

--- app/src/main/java/com/farmerbb/notepad/adapter/NoteListDateAdapter.java
+++ app/src/main/java/com/farmerbb/notepad/adapter/NoteListDateAdapter.java
@@ -18,19 +18,15 @@ package com.farmerbb.notepad.adapter;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import com.farmerbb.notepad.R;
+import com.farmerbb.notepad.managers.ThemeManager;
 import com.farmerbb.notepad.util.NoteListItem;
-
 import java.util.ArrayList;
-
-import androidx.core.content.ContextCompat;
 
 public class NoteListDateAdapter extends ArrayAdapter<NoteListItem> {
     public NoteListDateAdapter(Context context, ArrayList<NoteListItem> notes) {
@@ -60,53 +56,12 @@ public class NoteListDateAdapter extends ArrayAdapter<NoteListItem> {
         SharedPreferences pref = getContext().getSharedPreferences(getContext().getPackageName() + "_preferences", Context.MODE_PRIVATE);
         String theme = pref.getString("theme", "light-sans");
 
-        if(theme.contains("light")) {
-            noteTitle.setTextColor(ContextCompat.getColor(getContext(), R.color.text_color_primary));
-            noteDate.setTextColor(ContextCompat.getColor(getContext(), R.color.text_color_secondary));
-        }
-
-        if(theme.contains("dark")) {
-            noteTitle.setTextColor(ContextCompat.getColor(getContext(), R.color.text_color_primary_dark));
-            noteDate.setTextColor(ContextCompat.getColor(getContext(), R.color.text_color_secondary_dark));
-        }
-
-        if(theme.contains("sans")) {
-            noteTitle.setTypeface(Typeface.SANS_SERIF);
-            noteDate.setTypeface(Typeface.SANS_SERIF);
-        }
-
-        if(theme.contains("serif")) {
-            noteTitle.setTypeface(Typeface.SERIF);
-            noteDate.setTypeface(Typeface.SERIF);
-        }
-
-        if(theme.contains("monospace")) {
-            noteTitle.setTypeface(Typeface.MONOSPACE);
-            noteDate.setTypeface(Typeface.MONOSPACE);
-        }
-
-        switch(pref.getString("font_size", "normal")) {
-            case "smallest":
-                noteTitle.setTextSize(12);
-                noteDate.setTextSize(8);
-                break;
-            case "small":
-                noteTitle.setTextSize(14);
-                noteDate.setTextSize(10);
-                break;
-            case "normal":
-                noteTitle.setTextSize(16);
-                noteDate.setTextSize(12);
-                break;
-            case "large":
-                noteTitle.setTextSize(18);
-                noteDate.setTextSize(14);
-                break;
-            case "largest":
-                noteTitle.setTextSize(20);
-                noteDate.setTextSize(16);
-                break;
-        }
+        ThemeManager.setTextColor(getContext(), theme, noteTitle);
+        ThemeManager.setTextColorDate(getContext(), theme, noteDate);
+        ThemeManager.setFont(pref, noteTitle);
+        ThemeManager.setFont(pref, noteDate);
+        ThemeManager.setFontSize(pref, noteTitle);
+        ThemeManager.setFontSizeDate(pref, noteDate);
 
         // Return the completed view to render on screen
         return convertView;

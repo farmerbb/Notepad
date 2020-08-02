6d28607814d642b45cb247da5e74f814c8da73e7
Max Große <max12.gro@gmail.com>
2020-08-02 19:47:31 +0200

    Add more methods and abstract further
    Added a lot more methods for certain recurring actions such as setting the fontSize, background color, etc.

--- app/src/main/java/com/farmerbb/notepad/managers/ThemeManager.kt
+++ app/src/main/java/com/farmerbb/notepad/managers/ThemeManager.kt
@@ -9,6 +9,8 @@ import android.graphics.Typeface
 import android.net.Uri
 import android.os.Build
 import android.os.FileUriExposedException
+import android.util.Log
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
@@ -19,105 +21,129 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import com.farmerbb.notepad.R
 import org.apache.commons.lang3.StringUtils
-import org.w3c.dom.Text
 import us.feras.mdv.MarkdownView
+import org.w3c.dom.Text
 
 object ThemeManager {
+    private const val TAG = "ThemeManager"
+    @JvmStatic
+    fun setBackgroundColor(context: Context, theme: String, noteViewEdit: View) {
+        when {
+            theme.contains("light") -> noteViewEdit.setBackgroundColor(ContextCompat.getColor(context, R.color.window_background))
+            else -> noteViewEdit.setBackgroundColor(ContextCompat.getColor(context, R.color.window_background_dark))
+        }
+    }
+
+    @JvmStatic
+    fun setTextColor(context: Context, theme: String, noteContents: TextView) {
+        when {
+            theme.contains("light") -> noteContents.setTextColor(ContextCompat.getColor(context, R.color.text_color_primary))
+            else -> noteContents.setTextColor(ContextCompat.getColor(context, R.color.text_color_primary_dark))
+        }
+    }
+
+    @JvmStatic
+    fun setTextColorDate(context: Context, theme: String, noteDate: TextView) {
+        when {
+            theme.contains("light") -> noteDate.setTextColor(ContextCompat.getColor(context, R.color.text_color_secondary))
+            else -> noteDate.setTextColor(ContextCompat.getColor(context, R.color.text_color_secondary_dark))
+        }
+    }
+
+    @JvmStatic
+    fun setFontSize(pref: SharedPreferences, noteContents: TextView) {
+        noteContents.textSize =
+            when (pref.getString("font_size", "normal")) {
+                "smallest"  ->  12f
+                "small"     ->  14f
+                "normal"    ->  16f
+                "large"     ->  18f
+                else        ->  20f
+            }
+    }
+
+    @JvmStatic
+    fun setFontSizeDate(pref: SharedPreferences, noteDate: TextView) {
+        noteDate.textSize =
+                when (pref.getString("font_size", "normal")) {
+                    "smallest"  ->  8f
+                    "small"     ->  10f
+                    "normal"    ->  12f
+                    "large"     ->  14f
+                    else        ->  16f
+                }
+    }
+
+    @JvmStatic
+    fun setFont(pref: SharedPreferences, noteContents: TextView) {
+        noteContents.typeface =
+           with((pref.getString("theme", "light-sans")!!)) { // double bang (!) operator indicates that pref.getString will never return null (since we have a default value)
+               when {
+                   contains("sans") -> Typeface.SANS_SERIF
+                   contains("serif") -> Typeface.SERIF
+                   else -> Typeface.MONOSPACE
+               }
+       }
+    }
+
+    @JvmStatic
+    fun getFontMarkdown(pref: SharedPreferences) : String {
+       return with((pref.getString("theme", "light-sans")!!)) { // double bang (!) operator indicates that pref.getString will never return null (since we have a default value)
+               when {
+                   contains("sans") -> "sans"
+                   contains("serif") -> "serif"
+                   else -> "monospace"
+               }
+       }
+    }
+    @JvmStatic
+    fun getTextColor(context: Context, theme: String) : Int {
+        return with(theme) {
+            when {
+                contains("light") -> ContextCompat.getColor(context, R.color.text_color_primary)
+                else -> ContextCompat.getColor(context, R.color.text_color_primary_dark)
+            }
+        }
+    }
 
     @JvmStatic
     fun applyTextSettings(activity: FragmentActivity, noteContents: EditText) {
         val pref: SharedPreferences = activity.getSharedPreferences(activity.packageName + "_preferences", Context.MODE_PRIVATE)
         val scrollView: ScrollView = activity.findViewById(R.id.scrollView1)
-        val theme: String = pref.getString("theme", "light-sans") ?: "hello"
+        val theme: String = pref.getString("theme", "light-sans")!! // theme will never be null
 
-        if (theme.contains("light")) {
-            noteContents.setTextColor(ContextCompat.getColor(activity, R.color.text_color_primary))
-            noteContents.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background))
-            scrollView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background))
-        }
-
-        if (theme.contains("dark")) {
-            noteContents.setTextColor(ContextCompat.getColor(activity, R.color.text_color_primary_dark))
-            noteContents.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background_dark))
-            scrollView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background_dark))
-        }
-        with(theme) {
-            when {
-                contains("sans") -> noteContents.typeface = Typeface.SANS_SERIF
-                contains("serif") -> noteContents.typeface = Typeface.SERIF
-                else -> noteContents.typeface = Typeface.MONOSPACE
-            }
-        }
-
-        when (pref.getString("font_size", "normal")) {
-            "smallest" -> noteContents.textSize = 12f
-            "small" -> noteContents.textSize = 14f
-            "normal" -> noteContents.textSize = 16f
-            "large" -> noteContents.textSize = 18f
-            "largest" -> noteContents.textSize = 20f
-        }
+        setBackgroundColor(activity, theme, scrollView)
+        setTextColor(activity, theme, noteContents)
+        setFont(pref, noteContents)
+        setFontSize(pref, noteContents)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
     @JvmStatic
     fun applyNoteViewTheme(activity: FragmentActivity, noteContents: TextView?, markdownView: MarkdownView?, resources: Resources) : String {
-        val pref: SharedPreferences = activity.getSharedPreferences(activity.getPackageName() + "_preferences", Context.MODE_PRIVATE)
+        val pref: SharedPreferences = activity.getSharedPreferences(activity.packageName + "_preferences", Context.MODE_PRIVATE)
         val scrollView: ScrollView = activity.findViewById(R.id.scrollView)
-        val theme: String? = pref.getString("theme", "light-sans")
-        var textSize = -1
+        val theme: String = pref.getString("theme", "light-sans")!!
+        Log.d(TAG, "current theme: $theme")
+        Log.d(TAG, "noteContents are null: ${noteContents == null}")
+        var fontFamily :String? = null
         var textColor = -1
+        var textSize = -1F
 
-        var fontFamily: String? = null
-
-        if (theme!!.contains("light")) {
-            if (noteContents != null) {
-                noteContents.setTextColor(ContextCompat.getColor(activity, R.color.text_color_primary))
-                noteContents.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background))
-            }
-            if (markdownView != null) {
-                markdownView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background))
-                textColor = ContextCompat.getColor(activity, R.color.text_color_primary)
-            }
-            scrollView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background))
+        if (noteContents != null) {
+            setTextColor(activity, theme, noteContents)
+            setBackgroundColor(activity, theme, noteContents)
+            setFont(pref, noteContents)
+            setFontSize(pref, noteContents)
+            textSize = noteContents.textSize
         }
-
-        if (theme!!.contains("dark")) {
-            if (noteContents != null) {
-                noteContents.setTextColor(ContextCompat.getColor(activity, R.color.text_color_primary_dark))
-                noteContents.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background_dark))
-            }
-            if (markdownView != null) {
-                markdownView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background_dark))
-                textColor = ContextCompat.getColor(activity, R.color.text_color_primary_dark)
-            }
-            scrollView.setBackgroundColor(ContextCompat.getColor(activity, R.color.window_background_dark))
+        if (markdownView != null) {
+            Log.d(TAG, "markdownView is not null")
+            setBackgroundColor(activity, theme, markdownView)
+            fontFamily = getFontMarkdown(pref)
+            textColor = getTextColor(activity, theme)
         }
-
-        if (theme!!.contains("sans")) {
-            if (noteContents != null) noteContents.setTypeface(Typeface.SANS_SERIF)
-            if (markdownView != null) fontFamily = "sans-serif"
-        }
-
-        if (theme!!.contains("serif")) {
-            if (noteContents != null) noteContents.setTypeface(Typeface.SERIF)
-            if (markdownView != null) fontFamily = "serif"
-        }
-
-        if (theme!!.contains("monospace")) {
-            if (noteContents != null) noteContents.setTypeface(Typeface.MONOSPACE)
-            if (markdownView != null) fontFamily = "monospace"
-        }
-
-        when (pref.getString("font_size", "normal")) {
-            "smallest" -> textSize = 12
-            "small" -> textSize = 14
-            "normal" -> textSize = 16
-            "large" -> textSize = 18
-            "largest" -> textSize = 20
-        }
-
-        if (noteContents != null) noteContents.textSize = textSize.toFloat()
-
+        setBackgroundColor(activity, theme, scrollView)
         var css = ""
         if (markdownView != null) {
             val topBottom = " " + (resources.getDimension(R.dimen.padding_top_bottom) / resources.getDisplayMetrics().density).toString() + "px"

67c3fef79233fa424d12d66314ad2cfda0052eea
mahfuj14 <mahfujhassan14@gmail.com>
2022-05-12 18:45:40 +0600

    java getter setter added

---
+++ app/src/main/java/classwork/getter_setter.java
@@ -0,0 +1,26 @@
+package classwork;
+
+public class getter_setter {
+    int a,b;
+
+    public getter_setter(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}

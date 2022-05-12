811b65b8786a0fd4683ba767888003e42e7f74a1
mahfuj14 <mahfujhassan14@gmail.com>
2022-05-12 19:16:13 +0600

    Java class added from branch br

---
+++ app/src/main/java/Branch/new_branch_br.java
@@ -0,0 +1,37 @@
+package Branch;
+
+public class new_branch_br {
+    String name;
+    int id;
+    int roll;
+
+    public new_branch_br(String name, int id, int roll) {
+        this.name = name;
+        this.id = id;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}

ed8ea2d0f6dfa5946dfe18532a6d770a556e7e2d
mahfuj14 <mahfujhassan14@gmail.com>
2022-05-18 07:16:56 +0600

    MOdified from main

--- app/src/main/java/classwork/getter_setter.java
+++ app/src/main/java/classwork/getter_setter.java
@@ -2,6 +2,21 @@ package classwork;
 
 public class getter_setter {
     int a,b;
+    int test_t;
+
+    public getter_setter(int a, int b, int test_t) {
+        this.a = a;
+        this.b = b;
+        this.test_t = test_t;
+    }
+
+    public int getTest_t() {
+        return test_t;
+    }
+
+    public void setTest_t(int test_t) {
+        this.test_t = test_t;
+    }
 
     public getter_setter(int a, int b) {
         this.a = a;

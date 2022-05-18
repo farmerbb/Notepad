525226f7030985a995792770ce2bd3fd82cc7fe2
mahfuj14 <mahfujhassan14@gmail.com>
2022-05-18 07:08:44 +0600

    modified new-branch-br from br2

--- app/src/main/java/Branch/new_branch_br.java
+++ app/src/main/java/Branch/new_branch_br.java
@@ -4,6 +4,32 @@ public class new_branch_br {
     String name;
     int id;
     int roll;
+    double cgpa;
+    int position;
+
+    public new_branch_br(String name, int id, int roll, double cgpa, int position) {
+        this.name = name;
+        this.id = id;
+        this.roll = roll;
+        this.cgpa = cgpa;
+        this.position = position;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 
     public new_branch_br(String name, int id, int roll) {
         this.name = name;

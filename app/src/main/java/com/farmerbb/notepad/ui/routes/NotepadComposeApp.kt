c4a361dcffd0df4ca5dc1af56bf2e63962b66d8d
Braden Farmer <farmerbb@gmail.com>
2022-09-27 23:58:44 -0600

    Fix behavior of save button - it should act like a toggle between view / edit

--- app/src/main/java/com/farmerbb/notepad/ui/routes/NotepadComposeApp.kt
+++ app/src/main/java/com/farmerbb/notepad/ui/routes/NotepadComposeApp.kt
@@ -241,11 +241,15 @@ private fun NotepadComposeApp(
     }
 
     var showSaveDialog by rememberSaveable { mutableStateOf(false) }
+    var isSaveButton by rememberSaveable { mutableStateOf(false) }
     var text: String by rememberSaveable { mutableStateOf(note.text) }
-    val onSave = {
-        vm.saveNote(note.id, text) { newId ->
-            navState = if (directEdit) Empty else View(newId)
-        }
+
+    fun updateNavState(id: Long) {
+        navState = if (directEdit || !isSaveButton) Empty else View(id)
+    }
+
+    val onSave: () -> Unit = {
+        vm.saveNote(note.id, text, ::updateNavState)
     }
 
     if(showSaveDialog) {
@@ -256,7 +260,7 @@ private fun NotepadComposeApp(
             },
             onDiscard = {
                 showSaveDialog = false
-                navState = if (directEdit) Empty else View(note.id)
+                updateNavState(note.id)
             },
             onDismiss = {
                 showSaveDialog = false
@@ -273,11 +277,13 @@ private fun NotepadComposeApp(
     val onDismiss = { showMenu = false }
     val onMoreClick = { showMenu = true }
 
-    val onSaveClick: () -> Unit = {
-        if (showDialogs) {
-            showSaveDialog = true
-        } else {
-            onSave()
+    val onSaveClick: (Boolean) -> Unit = {
+        isSaveButton = it
+
+        when {
+            text == note.text -> updateNavState(note.id)
+            showDialogs ->  showSaveDialog = true
+            else ->  onSave()
         }
     }
 
@@ -306,7 +312,7 @@ private fun NotepadComposeApp(
                 vm.clearSelectedNotes()
             }
 
-            navState is Edit && text.isNotEmpty() -> onSaveClick()
+            navState is Edit && text.isNotEmpty() -> onSaveClick(false)
 
             else -> {
                 navState = Empty
@@ -459,7 +465,7 @@ private fun NotepadComposeApp(
             }
             backButton = { BackButton(onBack) }
             actions = {
-                SaveButton(onSaveClick)
+                SaveButton { onSaveClick(true) }
                 DeleteButton(onDeleteClick)
                 NoteViewEditMenu(
                     showMenu = showMenu,
